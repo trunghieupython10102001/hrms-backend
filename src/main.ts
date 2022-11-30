7d91d62c22a2005d9015a5adb356d02a7da1297d
dtviet <ex-vietdoanthe001@ocft.com>
2022-11-30 22:10:14 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const config = new DocumentBuilder()
     .setTitle('HRMS API')
